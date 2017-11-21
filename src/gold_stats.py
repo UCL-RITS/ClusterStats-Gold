0ad2d02b09b6997c62012dec797eda6f393e3f22
Heather Kelly <h.kelly@ucl.ac.uk>
2017-11-21 16:08:02 +0000

    Argument parsing and csv writing

--- src/gold_stats.py
+++ src/gold_stats.py
@@ -6,18 +6,27 @@ import sqlite3
 import datetime
 import argparse
 import configparser
+import csv
 
 def getargs(argv):
     parser = argparse.ArgumentParser(description="Show allocation usage data from the Gold database")
-    parser.add_argument("-i", "--institutes", help="Get usage data for all institutes", action='store_true')
+    parser.add_argument("--institutes", help="Get usage data for all institutes", action='store_true')
+    parser.add_argument("--csv", metavar="csvfile", help="Write results to csv, with the given filename")
 
+    # Show the usage if no arguments are supplied
+    if len(sys.argv[1:]) < 1:
+        parser.print_usage()
+        exit(1)
 
+    # return the arguments
+    # contains only the attributes for the main parser and the subparser that was used
+    return parser.parse_args()
 # end getargs
 
 # Get allocation usage data for all institutes for all time.
 # Order by allocation start, end and then project name 
 # (as first allocation all starts on same date).
-def gold_by_allocation_period(cursor):
+def gold_by_allocation_period_query():
     query = ("""SELECT j.g_project, sum(j.g_charge), r.g_id as date_alloc, r.g_account,
                        a.g_start_time, a.g_end_time, count(*) as num_jobs
                 FROM g_job AS j
@@ -27,7 +36,7 @@ def gold_by_allocation_period(cursor):
                   ON date_alloc = a.g_id
                 GROUP BY date_alloc 
                 ORDER BY a.g_start_time, a.g_end_time, j.g_project""")
-    cursor.execute(query)
+    return query
 
 
 # Main function
@@ -42,11 +51,11 @@ if __name__ == '__main__':
         exit(1)
 
     # read the config file    
-    config = configparser.ConfigParser()
-    config.read('config/gold.cnf')
-    db = config['Gold']['SQLITE_DB']
-
-    except ValueError as err:
+    try:
+        config = configparser.ConfigParser()
+        config.read('config/gold.cnf')
+        db = config['Gold']['SQLITE_DB']
+    except configparser.Error as err:
         print(err)
         exit(1)
 
@@ -55,7 +64,18 @@ if __name__ == '__main__':
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
 
-    except sqlite3.Error as err:
+        if (args.institutes):
+            cursor.execute(gold_by_allocation_period_query())
+
+        rows = cursor.fetchall()
+
+        # may want to process data in some way before output?
+
+        if (args.csvfile != None):
+            csvwriter = csv.writer(open(csvfile, newline='', encoding='utf-8', "w"))
+            csvwriter.writerows(rows)
+
+    except (sqlite3.Error, csv.Error) as err:
         print(err)
         exit(1)
     else:
